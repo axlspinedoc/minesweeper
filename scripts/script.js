6ca0a8d607ab3ca61e108fcf4d2d7476fe87d5fa
axlspinedoc <axel_robot@hotmail.com>
2022-03-12 12:28:35 -0600

    moving functions as FE

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,38 @@ const canvas = document.getElementById("canvas");
 
 start.addEventListener('click', handleStart);
 
+// function expressions
+const addBombs = function(table, qty){
+    
+    let remaining = qty;
+    // Make a copy of inbound table to make it iterable
+    const _rows = table.length;
+    const _columns = table[0].length;
+    const _grid = Array(_rows).fill().map(()=>Array(_columns).fill(0));
+    while(remaining>0){
+        const bombRow = parseInt(Math.random() * _rows);
+        const bombCol = parseInt(Math.random() * _columns);            
+        if(_grid[bombRow][bombCol] == 0){
+            remaining--;
+            _grid[bombRow][bombCol] = 1;
+        }        
+    }    
+    return _grid;
+}
+
+const countBombs = function(table, x, y){
+    let remaining = qty;
+    // Make a copy of inbound table to make it iterable
+    const _rows = table.length;
+    const _columns = table[0].length;
+    const _grid = Array(_rows).fill().map(()=>Array(_columns).fill(0));
+    //let sum = 0;
+    // Try copying a portion of the original table and adding the contents
+    const subArray = [1, 2, 3];
+    const sum = subArray.reduce((partialSum, a) => partialSum + a, 0);
+    console.log(sum); // 6
+}
+
 // Creates game grid and adds event listeners to each cell
 function handleStart() {    
     
@@ -23,19 +55,7 @@ function handleStart() {
     let grid = Array(rows).fill().map(()=>Array(columns).fill(0));
     // add bombs
     
-    grid = addBombs(grid, bombs);
-    
-    function addBombs (table, qty){
-        let remaining = qty;        
-            const bombRow = parseInt(Math.random() * rows);
-            const bombCol = parseInt(Math.random() * columns);            
-            if(table[bombRow][bombCol] == 0){
-                remaining--;
-                table[bombRow][bombCol] = 1;
-            }        
-        if(remaining > 0) addBombs(table, remaining);        
-        return table;
-    }        
+    grid = addBombs(grid, bombs);    
     
     console.table(grid);
 
@@ -70,6 +90,6 @@ function handleStart() {
 function checkCell (x, y, grid){
     const cell = document.getElementById(`R${x}C${y}`);    
     cell.className = 'columnVisited'
-    console.assert(typeof grid[10][10] !== 'undefined', "hello");
+    // console.assert(typeof grid[10][10] !== 'undefined', "hello");
     
 }
